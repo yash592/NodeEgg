b8d8e3dab65c0e1e4444b0e9c972ad3b569a7422
Yash <yash592@gmail.com>
2017-11-29 07:56:05 -0800

    sql added

---
+++ sql.js
@@ -0,0 +1,11 @@
+CREATE table inventory(
+
+id INTEGER AUTO_INCREMENT NOT NULL,
+product_name varchar(50) NOT NULL,
+department_name varchar(50) NOT NULL,
+price INTEGER (10, 3) NOT NULL,
+stock_quantity INTEGER
+
+)
+
+DESCRIBE inventory,
