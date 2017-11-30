a54228436f8361b0d18d82a707aa4c45b4e36b9b
Yash <yash592@gmail.com>
2017-11-29 18:27:59 -0800

    beeBay done

--- beeBay.js
+++ beeBay.js
@@ -21,7 +21,66 @@ var connection = mysql.createConnection ({
 connection.connect(function(err) {
 	if(!err) {
 		console.log("Connected!");
-		
+		showProducts();
 		
 	}
 });
+
+// =====================================
+
+// ShOW INVENTORY
+
+// =====================================
+
+function showProducts() {
+
+	console.log("WELCOME TO THE REVOLUTIONARY NODE SHOPPING STORE beeBAY! \n")
+
+	var query = connection.query("SELECT * FROM inventory", function(err, res) {
+
+		if (err) throw err
+
+		for(var i = 0; i < res.length; i++) {
+
+			
+
+		};
+
+		console.table(res);
+
+		buyAProduct();
+
+
+	});
+
+
+  
+
+}
+
+function buyAProduct() {
+
+	inquirer.prompt([
+
+	{
+		type: "input",
+		name: "userBuy",
+		message: "Enter the Item ID of the item you'd like to buy!"
+
+	},
+
+	{
+		type: "input",
+		name: "userNum",
+		message: "Enter the quantity of the item you'd like to buy!"
+
+	}
+      
+
+		]).then(function(response){
+
+			console.log("Bought!");
+
+		})
+}
+
